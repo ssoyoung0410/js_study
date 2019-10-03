92965b18322800dd52e07fedab2101d2d0fa6828
ssoyoung0410 <53144918+ssoyoung0410@users.noreply.github.com>
2019-10-03 15:12:58 +0900

    Update 반응속도.js

--- 반응속도.js
+++ 반응속도.js
@@ -26,10 +26,10 @@ var 타임아웃;
 
     } else if (스크린.classList.contains('now')) { //초록색 --> 파란색
        끝시간 = new Date(); //초록색으로 바뀌고 내가 스크린을 클릭하여 파란색으로 바뀌는 그 시간이 끝시간이 됨 .
-      const time = body.createElement('div');
-      time.textContent = '반응속도' + 끝시간 - 시작시간+'ms';
-      document.body.append(time);
-      //console.log('반응속도', 끝시간 - 시작시간,'ms');
+      //const time = body.createElement('div');
+      //time.textContent = '반응속도' + 끝시간 - 시작시간+'ms';
+      //document.body.append(time);
+      console.log('반응속도', 끝시간 - 시작시간,'ms');
       기록.push(끝시간 - 시작시간);
       시작시간 = null;
       끝시간 = null;
